3d4ebec561b38687e4c900ec583c32cc1d3507c2
Anh Nguyen <nguyena.r216@gmail.com>
2021-03-19 18:58:26 -0400

    set up home feed; query data

--- parstagram/feedViewController.swift
+++ parstagram/feedViewController.swift
@@ -7,18 +7,59 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class feedViewController: UIViewController {
+class feedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // get query
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
     }
     
     @IBAction func onLogoutButton(_ sender: Any) {
         PFUser.logOut()
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! postCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        
+        // set up cell
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af.setImage(withURL: url)
+        return cell
     }
     
 
---
+++ parstagram/postCell.swift
@@ -0,0 +1,27 @@
+//
+//  postCell.swift
+//  parstagram
+//
+//  Created by Anh Nguyen on 3/19/21.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
