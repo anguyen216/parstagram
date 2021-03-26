7fc219eeea3ee0bf8f36acc44ed16c5b3f2df18d
Anh Nguyen <nguyena.r216@gmail.com>
2021-03-26 00:41:36 -0400

    add fake comments

--- parstagram/feedViewController.swift
+++ parstagram/feedViewController.swift
@@ -62,6 +62,22 @@ class feedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        post.add(comment, forKey: "comments")
+        post.saveInBackground { (success, error) in
+            if success {
+                print("comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
